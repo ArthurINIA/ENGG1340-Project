2db4320e665fab8721e5daf06048cefee4ddcdb0
IORIshizuku <124580681+IORIshizuku@users.noreply.github.com>
2023-03-31 23:09:43 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,32 @@
+void processAction(string cmd)
+{
+
+    // interface 1
+    // interface 2
+    …
+        //interface 3
+…
+    // interface 4
+}
+
+for (int round = 0; round < 50; round++)
+{
+    for (int curCountry : countryList)
+    {
+        pick_random_event();
+
+        // let player make decision;
+        while (getline(cin, cmd))
+        {
+            if (cmd == “end”)
+            {
+                // may need to print sth
+                break;
+            }
+            processAction(cmd);
+        }
+
+        round_result();
+    }
+}
+end_game();
