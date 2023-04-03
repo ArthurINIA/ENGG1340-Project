5b4e4a9910dfab98102c65ef8efe40ca0d6b93b7
IORIshizuku <whitewind1995@gmail.com>
2023-04-03 23:02:26 +0800

    test struct of resouces
    modified main.cpp and interface_2.cpp to test, failed due to arm64

--- interface_2.cpp
+++ interface_2.cpp
@@ -1,6 +1,22 @@
 #include "all_interface.h"
+#include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 
-void run_interface_2(vector<string> &cmd){
-    cout << "interface 2" << endl; //testing
+void run_interface_2(vector<string> &cmd, int &population)
+{
+    // string internal_2_command;
+    if (cmd[2] == "to")
+    {
+        if (cmd[3] == " ")
+        {
+            if (population > 10000)
+            {
+                // do something
+                cout << population; // testing
+            }
+        }
+    }
+    cout << "interface 2" << endl; // testing
 }
--- main.cpp
+++ main.cpp
@@ -5,68 +5,90 @@
 #include "all_interface.h"
 using namespace std;
 
-//list of functions
-//debug message printer
-void testing(){
+struct resouces
+{
+    int population = 10000;
+    int food = 350;
+    int fuel = 100;
+    int metal = 100;
+} player;
 
+// list of functions
+// debug message printer
+void testing()
+{
 }
-vector<string> split(string raw_line){
-//split the string by space and return a vecetor of strings
+vector<string> split(string raw_line)
+{
+    // split the string by space and return a vecetor of strings
     vector<string> rt;
     string cur = "";
-    for(char i: raw_line){
-        if(i != ' ')
+    for (char i : raw_line)
+    {
+        if (i != ' ')
             cur += i;
-        else{
-            //if space, add the current string to the return vector, then clear the current string;
+        else
+        {
+            // if space, add the current string to the return vector, then clear the current string;
             rt.push_back(cur);
             cur = "";
         }
     }
-    //boundary case of the last string
-    if(cur != "") rt.push_back(cur);
+    // boundary case of the last string
+    if (cur != "")
+        rt.push_back(cur);
     return rt;
 }
 
 string countryList[] = {"Player", "PC1", "PC2", "PC3"};
 
-int main(){
+int main()
+{
     printIntro();
-    for (int round = 0; round < 50; round++){
-        for (string curCountry : countryList){
+    for (int round = 0; round < 50; round++)
+    {
+        for (string curCountry : countryList)
+        {
             pick_random_event();
 
-            if(curCountry == "Player"){
-                //read command line from player
+            if (curCountry == "Player")
+            {
+                // read command line from player
                 string raw_cmd;
-                while (getline(cin, raw_cmd)){
+                while (getline(cin, raw_cmd))
+                {
                     vector<string> cmd = split(raw_cmd);
-                    if(cmd[0] == "to"){
-                        if(cmd[1] == "i1" || cmd[1] == "admin-panel")
+                    if (cmd[0] == "to")
+                    {
+                        if (cmd[1] == "i1" || cmd[1] == "admin-panel")
                             run_interface_1(cmd);
-                        else if(cmd[1] == "i2" || cmd[1] == "internal")
-                            run_interface_2(cmd);
-                        else if(cmd[1] == "i3" || cmd[1] == "world-map")
+                        else if (cmd[1] == "i2" || cmd[1] == "internal")
+                            run_interface_2(cmd, player.population);
+                        else if (cmd[1] == "i3" || cmd[1] == "world-map")
                             run_interface_3(cmd);
-                        else if(cmd[1] == "i4" || cmd[1] == "world-news")
+                        else if (cmd[1] == "i4" || cmd[1] == "world-news")
                             run_interface_4(cmd);
                         else
                             cout << "This interface does not exist!" << endl;
                     }
-                    else if(cmd[0] == "end"){
+                    else if (cmd[0] == "end")
+                    {
                         cout << "Today has come to an end." << endl;
                         break;
                     }
-                    else if(cmd[0] == "quit"){
-                        //save game
+                    else if (cmd[0] == "quit")
+                    {
+                        // save game
                         cout << "Game Saved" << endl;
-                        //close game
+                        // close game
                         exit(0);
                     }
-                    else if(cmd[0] == "help"){
-                        //manual
+                    else if (cmd[0] == "help")
+                    {
+                        // manual
                     }
-                    else{
+                    else
+                    {
                         cout << "Invalid Operation!" << endl;
                     }
                 }
@@ -77,30 +99,16 @@ int main(){
     end_game();
 }
 
-void printIntro(){
+void printIntro()
+{
     string introStory[] = {
-        "In the last century, the Earth was overdeveloped by human civilization, and the energy catastrophe and the pollution storm were unprecedented destructive forces that reverted to the civilized world."
-        ,"The catastrophe not only set back human civilization in terms of technology and desertification of most of the land, but also brought about a horrific change to the earth - \"a black miasma covering the outer atmosphere, with daylight no longer within reach\"."
-        ,"Talents spring up in the turbulent days, your country's rising stars of the future have been put into new energy research, cold fusion technology is about to be completed."
-        ,"The countries are struggling for land, searching for rare raw energy under the region, for the sake of national peace and security."
-        ,"You, as a commander, please keep your resources intact and hold the border until the cold fusion arrives."
-        ,"......"
-        ,"......"
-        ,"01-Jan-2023"
-        ,"  Nuclear Department Head: Greetings sir, I have a good news to you."
-        ,"  Nuclear Department Head: The testing phase of the cold fusion has come to the last step."
-        ,"                      You: How many days are needed?"
-        ,"  Nuclear Department Head: 50 more days before our new era."
-        ,"  Nuclear Department Head: Please protect us from the neighbouring threats."
-        ,"......"
-        ,"......"
-        ,"Can you keep your country alive in the next 50 days?"
-    };
-    for(string x: introStory) cout << x << endl;
+        "In the last century, the Earth was overdeveloped by human civilization, and the energy catastrophe and the pollution storm were unprecedented destructive forces that reverted to the civilized world.", "The catastrophe not only set back human civilization in terms of technology and desertification of most of the land, but also brought about a horrific change to the earth - \"a black miasma covering the outer atmosphere, with daylight no longer within reach\".", "Talents spring up in the turbulent days, your country's rising stars of the future have been put into new energy research, cold fusion technology is about to be completed.", "The countries are struggling for land, searching for rare raw energy under the region, for the sake of national peace and security.", "You, as a commander, please keep your resources intact and hold the border until the cold fusion arrives.", "......", "......", "01-Jan-2023", "  Nuclear Department Head: Greetings sir, I have a good news to you.", "  Nuclear Department Head: The testing phase of the cold fusion has come to the last step.", "                      You: How many days are needed?", "  Nuclear Department Head: 50 more days before our new era.", "  Nuclear Department Head: Please protect us from the neighbouring threats.", "......", "......", "Can you keep your country alive in the next 50 days?"};
+    for (string x : introStory)
+        cout << x << endl;
 }
 
-void pick_random_event(){
-
+void pick_random_event()
+{
 }
 
 void processAction(string cmd)
@@ -112,10 +120,10 @@ void processAction(string cmd)
     */
 }
 
-void round_result(){
-
+void round_result()
+{
 }
 
-void end_game(){
-
+void end_game()
+{
 }
