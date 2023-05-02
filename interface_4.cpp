e9ef6d9063d040ee1ba7cb011627a607bcb573ef
IORIshizuku <whitewind1995@gmail.com>
2023-05-02 22:09:58 +0800

    Added UI function in interface4
    interface4.cpp: transplanted UI function to i4. added cal_result function. test will conduct after solving i3 critical error.

--- interface_4.cpp
+++ interface_4.cpp
@@ -1,6 +1,47 @@
 #include "all_interface.h"
+#include "UI.h"
 using namespace std;
 
-void run_interface_4(vector<string> &cmd){
-    cout << "interface 4" << endl; //testing
+void show_round_result(vector<string> content);
+void cal_result();
+
+void run_interface_4(vector<string> &cmd)
+{
+    // cout << "interface 4" << endl; // testing
+    cal_result();
+
+    vector<string> content; // contains all result
+
+    show_round_result(content);
+}
+
+void show_round_result(vector<string> content)
+{
+    // cout << "+-------------------------------------------------------------------------------------+" << endl;
+    //  cout << "|";
+    UI gameScreen;
+    string s;
+    string str1 = "Food: " + to_string(player.food), str2 = "Fuel: " + to_string(player.fuel), str3 = "Metal: " + to_string(player.metal), str4 = "Population: " + to_string(player.population);
+    // cout << content << endl;
+    vector<string> vec1 = {str1}, vec2 = {str2}, vec3 = {str3}, vec4 = {str4};
+    gameScreen.divide(1, 1, 120, 5, "resource-bar");
+    gameScreen.divide(1, 1, 40, 5, "interface-name");
+    gameScreen.divide(40, 1, 60, 5, "resource-1");
+    gameScreen.divide(60, 1, 80, 5, "resource-2");
+    gameScreen.divide(80, 1, 100, 5, "resource-3");
+    gameScreen.divide(100, 1, 120, 5, "resource-4");
+    gameScreen.divide(1, 5, 40, 29, "manual");
+    gameScreen.divide(40, 5, 120, 29, "game-content");
+
+    gameScreen.drawAll("interface-name", "center", {"Interface 4: Round Result"});
+    gameScreen.drawAll("resource-1", "center", vec1);
+    gameScreen.drawAll("resource-2", "center", vec2);
+    gameScreen.drawAll("resource-3", "center", vec3);
+    gameScreen.drawAll("resource-4", "center", vec4);
+    gameScreen.drawAll("game-content", "center", content);
+    gameScreen.drawLineStart("manual");
+    gameScreen.drawLine("center", "user manual");
+    gameScreen.drawLine("center", "sdfsdfsdf");
+    gameScreen.drawLineStop();
+    gameScreen.print();
 }
