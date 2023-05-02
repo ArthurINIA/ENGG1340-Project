0f1424fa5b9ce996415776dfea780403255c4da0
flyuzu <125441595+flyuzu@users.noreply.github.com>
2023-05-02 22:19:00 +0800

    Update randomevent.cpp

--- randomevent.cpp
+++ randomevent.cpp
@@ -8,32 +8,33 @@ int event(){
     cout << eventl[randomevent] << endl;
     if (randomevent == 0){
         int eventpick = rand()%3;
+        int eventpick = rand()%3;
         if (eventpick == 0){
-            maplist[0].allianceval += 50;
+            country[3][2] += 100;
         }
         if (eventpick == 1){
-            maplist[3].allianceval += 50;
+            country[12][2] += 100;
         }
         if (eventpick == 2){
-            maplist[15].allianceval += 50;
+            country[15][2] += 100;
         }
         
     }
     if (randomevent == 2){
-        agriculture -=200;
-        economy -= 300;
+        player.food -=200;
+        player.metal *= 0.8;
     }
     if (randomevent == 4){
-        agriculture -= 700;
+        player.food -= 700;
     }
     if (randomevent == 6){
-        player.millitary -= 1000;
+        player.millitary_factor -= 1000;
     }
     if (randomevent == 8){
-        manpower -= 100000;
+        player.population -= 100000;
     }
     if (randomevent == 9){
-        oil_refinery -= 1;
+        play.fule *= 0.7;
     }
     if (randomevent == 10){
         int eventpick = rand()%3;
@@ -48,7 +49,7 @@ int event(){
         }
     }
     if (randomevent == 11){
-        economy *= 1.1;
+        player.metal *= 1.1;
     }
     if (randomevent == 13){
         int eventpick = rand()%3;
@@ -63,18 +64,18 @@ int event(){
         }
     }
     if (randomevent == 14){
-        millitaryval *= 1.2;
+        player.millitary_factor *= 1.2;
     }
     if (randomevent == 17){
-        agriculture *= 1.3;
-        economy *= 1.1;
+        player.food *= 1.3;
+        player.metal *= 1.1;
     }
     if (randomevent == 18){
-        manpower *= 0.5;
+        player.population *= 0.5;
     }
     if (randomevent == 19){
-        player.millitary *= 1.2;
-        manpower *= 0.9;
+        player.millitary_factor *= 1.2;
+        player.population *= 0.9;
     }
     return 0;
 }
